20d483effbc34eede7c0bba2eb1247e20df32f60
= <sutiyosoahmadi@gmail.com>
2020-10-16 16:09:14 +0700

    belajar konversi tipe data

--- src/Konversi01.kt
+++ src/Konversi01.kt
@@ -1,3 +1,13 @@
 fun main(){
-    var hargaMobil : Int
+    var hargaMobil: Int = 250_000_000
+    var hargaMotor: Long = hargaMobil.toLong()
+
+    println(hargaMobil)
+    println(hargaMotor)
+
+    when(hargaMobil){
+        is Int -> print("Harga Mobil adalah Int")
+        !is Int -> print("Harga Mobil adalah Long")
+    }
+
 }
